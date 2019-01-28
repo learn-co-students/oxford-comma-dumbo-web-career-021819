65e9ec415b9cc534a86d681be0e4009295f8ac9d
CHRISTOPHER TRUONG <christophertruongm@gmail.com>
2019-01-28 23:39:21 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,21 @@
 def oxford_comma(array)
-
+  finalStr = ''
+  
+  if array.length == 1
+    finalStr += array[0]
+  elsif array.length == 2
+    finalStr += array[0] + " and " + array[1]
+  else
+    i = 0
+    while i < array.length - 1
+      current = array[i]
+      
+      finalStr += current + ', '
+    
+    i += 1
+    end
+    finalStr += 'and ' + array[-1]
+  end
+  
+  return finalStr
 end
