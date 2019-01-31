676e67b5958469bd1a3b788fe10ee3920f2734fd
1-8192 <allegranzia@gmail.com>
2019-01-31 10:51:34 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return array.join
+  elsif array.length == 2
+    return array.join(" and ")
+  else
+    last_item = array.pop
+    string = array.join(", ")
+    string << ", and #{last_item}"
+    return string
+  end
 end
