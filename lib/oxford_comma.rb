e3b9e0937ffe08aeb7a6df6eead576e60da2eb53
Antoinette Lee <antoinette.lee02@myhunter.cuny.edu>
2019-01-31 10:36:31 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    string = array.join
+  elsif array.size == 2
+    string = array.join(" and ")
+  elsif array.size >= 3
+    array[-1] = "and #{array[-1]}" 
+    string = array.join(", ")
+  else
+    string = ""
+  end
+  return string
 end
+
+
+
