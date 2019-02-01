79fe34cacc4d9a46187b5f8f7ac4d4270a268d29
peidansauyukjuk <smei8800@gmail.com>
2019-02-01 16:39:30 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  case array.size
+  when 1
+    return array[0]
+  when 2
+    return array.join(" and ")
+  else
+    and_last_item = "and " + array.pop
+    array.push(and_last_item)
+    return array.join(", ")
+  end
 end
