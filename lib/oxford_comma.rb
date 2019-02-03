0f321fa5406b3d569471f8817cc7bfc94bccf5f2
Jing Cao <jingcao.yale@gmail.com>
2019-02-03 22:41:39 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
+  if array.length() == 1
+    return array[0]
+
+  elsif array.length() ==2
+    array[-2] = (array[-2] + " and ")
+    return array.join
+
+  elsif array.length() > 2
+    commas = array[0..(array.length()-2)].collect { |f| f + ", " }
+    return commas.join + "and " + array[-1]
+  end
 
 end
