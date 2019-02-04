9c1e87bea04574c316c01ff863dd0afa5e75e918
edwin ramos <edwinramos269@gmail.com>
2019-02-04 21:37:12 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  array2=[]
+ if array.length==1
+  return "#{array[0]}"
+ elsif array.length==2 
+   return "#{array[0]} and #{array[1]}"
+ elsif array.length==3
+   return "#{array[0]}, #{array[1]}, and #{array[2]}"
+ else 
+   array[-1].insert(0, "and ")
+   return array.join(", ")
+ end
 end
