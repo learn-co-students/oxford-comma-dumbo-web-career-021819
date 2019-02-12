38e225a43a990a2999d876795c16939f6f2715df
Joshua Rivera-Sampayo <josh.rs1199@gmail.com>
2019-02-12 04:58:11 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+ if array.length == 1
+   return array.join
+ elsif array.length == 2
+   return array.join(" and ")
+ elsif array.length > 2
+   sentence = array
+   sentence[-1] = "and #{sentence.last}"
+   return sentence.join(", ")
+ end
 end
