38f1ec2d437d3ae115df94a4813de901e3225f9e
Ashley Archibald <aarchibald20@gmail.com>
2019-02-15 00:01:38 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
-end
+  if array.length < 2
+      array.join
+   elsif array.length == 2
+     array.insert(1, "and").join(" ")
+   else
+     string = ", and " + array.pop
+     string = array.join(", ") << string
+   end
+ end
